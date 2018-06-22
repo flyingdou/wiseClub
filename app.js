4d341efbc2d8fd02f0a25640bacd6fb5c644192f
flyingdou <1576883859@qq.com>
2018-06-22 16:20:17 +0800

    sd

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ App({
   constant:{
      base_pic_url:'https://www.ecartoon.com.cn/picture/',
      base_img_url:'https://fish.ecartoon.com.cn/img/'
-
   },
   request_url:'https://www.ecartoon.com.cn/wiseclubmp!'
 
