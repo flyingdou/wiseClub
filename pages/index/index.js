6bd47f32702da8070dc3377a59df7384ffc1b50a
w1441639547 <1441639547@qq.com>
2018-06-25 14:13:08 +0800

    修改首页遮罩 , 弃用坐标转换
    2018-06-25 14:13

--- pages/index/index.js
+++ pages/index/index.js
@@ -186,7 +186,7 @@ Page({
      * 转换俱乐部列表数据中的坐标为腾讯系坐标
      */
     var convertPoint = (club) => {
-      // 调用util方法进行坐标转换(百度坐标系转换为腾讯坐标系)
+      // 调用util方法进行坐标转换(百度坐标系转换为腾讯坐标系)(弃用)
       var point = util.BdmapEncryptToMapabc(club.latitude, club.longitude);
       club.latitude = point.lat;
       club.longitude = point.lng;
@@ -195,7 +195,7 @@ Page({
 
     // 去地图中
     var clubList = [this.data.club];
-    wx.setStorageSync('clubList', clubList.map(convertPoint));
+    wx.setStorageSync('clubList', clubList);
     wx.navigateTo({
       url: '../map/map'
     });
@@ -374,14 +374,14 @@ Page({
       if (location_result.refuse) {
         return;
       }
-      // 坐标系转换
-      var point = util.MapabcEncryptToBdmap(location_result.latitude, location_result.longitude);
+      // 坐标系转换(弃用)
+      // var point = util.MapabcEncryptToBdmap(location_result.latitude, location_result.longitude);
       // 请求参数
       let param = {
         memberId: wx.getStorageSync('memberId'),
         clubId: wx.getStorageSync('clubId'),
-        longitude: point.lng,
-        latitude: point.lat
+        longitude: location_result.longitude,
+        latitude: location_result.latitude
       }
       // 调用服务端签到接口
       var signTime = await wxApi.getServerTime();
